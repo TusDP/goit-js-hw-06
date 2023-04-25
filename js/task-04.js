93a9633f92ac1a0d530356b86008c0fa8d75b520
Artur Yaroshenko <120420695+TusDP@users.noreply.github.com>
2023-04-25 23:19:49 +0300

    Done task#4

---
+++ js/task-04.js
@@ -0,0 +1,26 @@
+// Лічильник складається зі спану і кнопок, які по кліку повинні збільшувати і зменшувати його значення на одиницю.
+//1) Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
+//2) Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
+//3) Оновлюй інтерфейс новим значенням змінної counterValue.
+
+let counterValue = 0;
+
+const refs = {
+  decrementBtn: document.querySelector('button[data-action="decrement"]'),
+  incrementBtn: document.querySelector('button[data-action="increment"]'),
+  value: document.querySelector('#value'),
+}
+
+
+refs.decrementBtn.addEventListener('click', () => {
+  counterValue -= 1;
+  refs.value.textContent = counterValue;
+});
+
+refs.incrementBtn.addEventListener('click', () => {
+  counterValue += 1;
+  refs.value.textContent = counterValue;
+});
+
+
+
